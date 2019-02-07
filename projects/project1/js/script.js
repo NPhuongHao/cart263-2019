19e42f643cc955864ac44b8769c1a4454f0cd4ea
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2019-02-07 15:09:34 -0500

    P1: Rotate the circle when right arrow is pressed, stop rotating when key is released

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -9,3 +9,39 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+let $circle3;
+let $circle2;
+let $circle1;
+
+let $avatar;
+
+$(document).ready(function() {
+  $circle3 = $('#c3');
+  $circle2 = $('#c2');
+  $circle1 = $('#c1');
+
+  $avatar = $('avatar');
+
+  let angle = .5;
+  let initialSize = 420;
+  const SIZE_LOSS = 10;
+
+  $(document).on('keydown', function(event) {
+    if (event.keyCode == 39) {
+      angle-=.5;
+
+      setInterval(function(){
+        $circle3.rotate(angle);
+
+      },50);
+
+    }
+  });
+  $(document).on('keyup', function(event) {
+    if (event.keyCode == 39) {
+      $circle3.stopRotate();
+    }
+  })
+
+});

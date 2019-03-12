7de6828da18f8b3df257ff08b8b75bf0c33023a9
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2019-03-12 12:26:32 -0400

    P2: Created a list of randomized options to choose from

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -2,8 +2,8 @@
 
 /*****************
 
-Title of Project
-Author Name
+CLOUDY WITH A CHANCE OF ACCURATE TITLE
+Nguyen PHuong Hao
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -53,7 +53,13 @@ function dataLoaded(data) {
   console.log("This data has been loaded");
   keywords = data.keywords;
 
-  newRound();
+  $(document).keypress(function(event) {
+    console.log('keypressed');
+    if (event.which == 13) {
+      console.log("omg");
+      newRound();
+    }
+  })
 }
 
 function newRound() {
@@ -77,7 +83,49 @@ function newRound() {
   }
 
   //Put that answer in a random position in the options array
+  //The position should not exceeding the number of available options
   let correctPosition = Math.floor(Math.random()*NUM_OPTIONS);
   options[correctPosition] = answer;
+
   console.log(options);
+
+  //Randomize 7 more titles to fill the options array
+  for (let i = 0; i<NUM_OPTIONS; i++) {
+    //if the current position is similar to the correct answer's position
+    if (i = correctPosition) {
+      //ignore it
+      i = i;
+    }
+    //if not, create a new array that has the first element similar to the correct answer's first element
+    //this is to create a daegree of confusion
+    else {
+      optionRandomizer(i)
+    }
+  }
+}
+
+function optionRandomizer(code) {
+  //create an array that store this option
+  let currentOption;
+  //assign the same first element of the answer array to this option's array
+  currentOption[i] = answer[0];
+  //then randomize the rest
+  for (var i=1; i<NUM_KEYWORDS; i++) {
+    //Add a random keyword in the list to the currentOption's current element
+    currentOption[i] = keywords[Math.floor(Math.random()*keywords.length)];
+    console.log(currentOption);
+    //Compare this element to the previous element
+    for (let t=0; t<i; t++){
+      //if the current element is similar to the element in comparison
+      if (answer[t] === answer[i]) {
+        //redo the process
+        i--;
+      } else {
+        //if not, keep processing
+        i = i;
+      }
+    }
+  }
+  //add this array to the options array
+  options(code) = currentOption;
 }

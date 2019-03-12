f80ceaa2cce6a770f93b362bb68cf2336a0498a6
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2019-03-12 10:35:19 -0400

    P2: Created keywords JSON file & basic setup

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -9,3 +9,14 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let keywords;
+
+$(document).ready(setup);
+
+function setup() {
+  $. getJSON('data/kidKeywords.json', dataLoaded);
+}
+
+function dataLoaded(data) {
+  console.log("This data has been loaded");
+}

e5a41602d4ef956e0a08be82c54b9298f3ffb76b
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2019-04-24 18:13:04 -0400

    P3: Added more comments

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -2,22 +2,28 @@
 
 /*****************
 
-Title of Project
-Author Name
+  MAYORING
+  Nguyen Phuong Hao
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+  A card game based on the gameplay of Spoopy Squad's Underhand.
+  In this game, you play as the mayor of a small village near the forest. Pressured by wars and the wolfs nearby,
+the mayor signed a contract with the Big Wolf, in order to minimize the damage.
+  Manage your resources and survive through the weeks, and maybe you'll be able to break the contract!
 
 ******************/
 
+//The amount of available options
 const NUM_OPTIONS = 3;
+//The total amount of types of resources
 const NUM_RESOURCES = 6;
 
+//Resource list
 //[Wolf's bane, Food, Money, Livestock, Villager, Risk]
 let resources = [0,2,2,2,2,0];
 let answer = [0,0,0,0,0,0];
 let currentOptions = [];
 
+//Array to store future special event and to avoid repetition
 let events = {
   last2w: {id: 'none'},
   last1w: {id: 'none'},
@@ -26,22 +32,19 @@ let events = {
   next2w: {id: 'none'},
 };
 
+//variables to store each type of events
 let $eventGain;
 let $eventLoss;
 let $eventTrade;
 let $eventSpecial;
 let $eventWolfsBane;
 
+//variable to count the week/round
 let week = 0;
 
+//FOR TEST ONLY
 let $nextRound;
 
-let debts = false;
-let famine = false;
-let desperation = false;
-let greed = false;
-let attack = false;
-let bigWolf = false;
 
 //Setup the program
 $(document).ready(setup);
@@ -58,14 +61,17 @@ function startGame() {
     $('hgroup').remove();
   });
 
+  //reveal the game's content
   $('.content').css("display", "block");
 
+  //FOR TEST
   $nextRound = $('<div id="nextRound">Next round</div>')
   $('#playground').append($nextRound);
   $nextRound.animate({
     opacity: '+=1'
   },1000);
 
+  //Load the events' data
   $.getJSON('data/events.json', dataLoaded);
 }
 
@@ -81,11 +87,32 @@ function dataLoaded(data) {
   $eventWolfsBane = data.eventWolfsBane;
 
   newRound();
-  displayAnswers();
 }
 
-function newRound() {
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
 
+//
+//MECHANICS EXPLAINED
+//
+//
+//1. Choose the week's event (can be a special event or a random event)
+//2. Set up 3 options corresponding to the current event
+//3. In each option, set up a set of requirements (can change according to the resources) and rewards
+//4. Player choose to give away a set of answer from their resources
+//5. If the answer corresponds to an option's requirements, the option is tagged fulfilled
+//6. If the player clicks on a fulfilled option, resources will change accordingly.
+//7. Depending on the player's current resources and last option, a special event can be triggered
+//8. If after a cycle of 30 rounds, the player can kill the Big Wolf, then the player wins the game.
+//9. If player is forced to choose a "game lost" option, the player loses the game.
+
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+//newRound()
+//
+//call a new round
+function newRound() {
   //check if the current state of resources call for any special event
   examineSpecialEvent();
   //Update data for the new week's situation
@@ -137,28 +164,49 @@ function newRound() {
 
 }
 
+
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+
+//answerChosen
+//
+//Modify the answer according to player's action
 function answerChosen() {
+  //if player clicks on the wolfsbane resource
   if ($(this).attr("id") == "resource0") {
+    //if this resource is > 0
     if (answer[0] < resources[0]) {
+      //add 1 unit to the corresponding answer
       answer[0]++;
     }
-  } else if ($(this).attr("id") == "resource1") {
+  }
+  //if player clicks on the food resource
+  else if ($(this).attr("id") == "resource1") {
     if (answer[1] < resources[1]) {
       answer[1]++;
     }
-  } else if ($(this).attr("id") == "resource2") {
+  }
+  //if player clicks on the money resource
+  else if ($(this).attr("id") == "resource2") {
     if (answer[2] < resources[2]) {
       answer[2]++;
     }
-  } else if ($(this).attr("id") == "resource3") {
+  }
+  //if player clicks on the livestock resource
+  else if ($(this).attr("id") == "resource3") {
     if (answer[3] < resources[3]) {
       answer[3]++;
     }
-  } else if ($(this).attr("id") == "resource4") {
+  }
+  //if player clicks on the villager resource
+  else if ($(this).attr("id") == "resource4") {
     if (answer[4] < resources[4]) {
       answer[4]++;
     }
-  } else if ($(this).attr("id") == "resource5") {
+  }
+  //if player clicks on the risk resource
+  else if ($(this).attr("id") == "resource5") {
     if (answer[5] < resources[5]) {
       answer[5]++;
     }
@@ -167,32 +215,33 @@ function answerChosen() {
   checkAnswer();
 }
 
+//answerChosen
+//
+//Take action according to player's decision
 function optionChosen() {
-  if ($(this).attr('id') == "option0"){
-    if ($(this).hasClass("fulfilled")) {
-      options[0].checkforSpecial();
-      options[0].updateResource();
-      newRound();
-    }
-  } else if ($(this).attr('id') == "option1") {
-    if($(this).hasClass("fulfilled")) {
-      options[1].checkforSpecial();
-      options[1].updateResource();
-      newRound();
-    }
-  } else if ($(this).attr('id') == "option2") {
-    if($(this).hasClass("fulfilled")) {
-      options[2].checkforSpecial();
-      options[2].updateResource();
-      newRound();
+  for (var i=0; i<NUM_OPTIONS; i++) {
+    //if player choses the i option
+    if ($(this).attr('id') == "option"+i){
+      if ($(this).hasClass("fulfilled")) {
+        //check if this option triggers any special event
+        options[i].checkforSpecial();
+        //update the resources accordingly
+        options[i].updateResource();
+        //call for a new round
+        newRound();
+      }
     }
   }
 }
 
+//answerModify
+//
+//Modify the player's answer if they click on an answer element
 function answerModify() {
   for (var i = 0; i<NUM_RESOURCES; i++) {
     if ($(this).attr('id') == "answer"+i) {
       //limit the answer's value between 0 and 15 (the maximum amount of cards)
+      //Reduce 1 unit from the corresponding answer
       answer[i] = range(answer[i]-1, 0, 15);
     }
   }
@@ -202,15 +251,37 @@ function answerModify() {
   displayAnswers();
 }
 
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+//displayResources()
+//
 function displayResources() {
+  //variables to store the resources' name
+  let resourceNames = ["Wolf's bane", "Food", "Money", "Livestock", "Villager", "Risk"]
   for (var i = 0; i<NUM_RESOURCES; i++) {
+    //Remove the old resource status
     $('#resource'+i).remove();
-    let resourceNames = ["Wolf's bane", "Food", "Money", "Livestock", "Villager", "Risk"]
+    //append the corresponding element into the resources HTML element
     let $content = $('<div class="resource" id="resource'+i+'">'+resourceNames[i]+': '+resources[i]+'</div>')
     $('.resources').append($content);
   }
 }
 
+//displayOptions()
+//
+//display the options corresponding to the current event
+function displayOptions() {
+  for (var i = 0; i<NUM_OPTIONS; i++) {
+    //remove the old content
+    $('#optionContent'+i).remove();
+    //add the new content
+    currentOptions[i].display(i);
+  }
+}
+
+//displayAnswers()
+//
 function displayAnswers() {
   for (var i = 0; i<NUM_RESOURCES; i++) {
     $('#answerContent'+i).remove();
@@ -219,6 +290,13 @@ function displayAnswers() {
   }
 }
 
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+//chooseEvent()
+//
+//Run the event randomizer
+//If the answer received is similar to the last two events, repeat the process
 function chooseEvent() {
   eventRandomizer();
   if (events.last2w.id == events.current.id || events.last1w.id == events.current.id) {
@@ -227,13 +305,16 @@ function chooseEvent() {
   }
 }
 
+//eventRandomizer()
+//
+//Randomize the current event
 function eventRandomizer() {
   var r = Math.random();
   var currentCard;
-  if (r <= 0.15) {
+  if (r <= 0.05) {
     //Open wolf's bane event
     currentCard = $eventWolfsBane[0];
-  } else if (r <= 0.35) {
+  } else if (r <= 0.30) {
     //Open a Loss event
     currentCard = $eventLoss[Math.floor(Math.random() * $eventLoss.length)];
   } else if (r <= 0.75) {
@@ -246,6 +327,9 @@ function eventRandomizer() {
   events.current = new Event(currentCard.id, currentCard.title, currentCard.description, currentCard.options)
 }
 
+//examineSpecialEvent()
+//
+//check if the current resources status calls for any special event
 function examineSpecialEvent() {
   let $newEvent = {id: 'none'};
   //trigger famine card if there's no food
@@ -283,6 +367,9 @@ function examineSpecialEvent() {
   }
 }
 
+//changeWeek()
+//
+//Update the new week's records
 function changeWeek() {
   events.last2w = events.last1w;
   events.last1w = events.current;
@@ -293,17 +380,8 @@ function changeWeek() {
   console.log("events: "+ events);
 }
 
-//displayOptions()
-//
-//display the options corresponding to the current event
-function displayOptions() {
-  for (var i = 0; i<NUM_OPTIONS; i++) {
-    //remove the old content
-    $('#optionContent'+i).remove();
-    //add the new content
-    currentOptions[i].display(i);
-  }
-}
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
 
 //checkAnswer()
 //
@@ -322,19 +400,30 @@ function checkAnswer() {
   }
 }
 
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+//gameOver()
+//
+//How the game reacts when the game is over
 function gameOver(cond) {
+  //remove the content
   $(".content").remove();
+  //FORTEST
   $nextRound.remove();
+  //If the game is won
   if (cond === "won") {
     $("#playground").append("<div class='result'>You win</div>");
   }
+  //If the game is lost
   else if (cond === "lost") {
     $("#playground").append("<div class='result'>You lose</div>");
   }
-  else {
-    return false;
-  }
 }
+
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
 
 //range()
 //
